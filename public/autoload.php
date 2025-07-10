27ef5df501e6dd46f92a364483930085b23470b0
n3mr1d <nameraid@protonmail.com>
2025-07-10 20:23:32 +0700

    Initial commit

--- public/autoload.php
+++ public/autoload.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
 $fetchdata = function($table) {
     global $sql;
     $stmt = $sql->prepare("SELECT * FROM $table");
--- public/resource/function/form.php
+++ public/resource/function/form.php
@@ -1,8 +1,11 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 // Show form login page
 function showadmin()
 {
+
     print_start("login");
 
     if (isset($_SESSION['errors'])) {
